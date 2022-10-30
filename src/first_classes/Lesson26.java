aab7269b32f977e5ed68af357f142b74ef3534fe
Maksym Kirichenko <enchmojos@gmail.com>
2022-10-30 19:54:17 +0200

    Lesson26_more

---
+++ src/first_classes/Lesson26.java
@@ -0,0 +1,39 @@
+package first_classes;
+
+public class Lesson26 {
+
+    public static void main(String[] args) {
+        /**
+         * OBJECT REFERENCES:
+         * - object - an instance of a class
+         * - reference - points to a memory location
+         *
+         * HOW TO CREATE AN OBJECT?
+         * - ClassName objectName = value OR new ClassName();
+         * Uppercase in class name beginning is NOT FORCED
+         *          with Java, but it is a BUSINESS
+         *          STANDART
+         *
+         *
+         */
+
+        Lesson25_Practice earthShakerSkilled = new Lesson25_Practice();
+
+        /**
+         * DIFFERENCE BETWEEN object reference AND primitive
+         * - OR can have functionality - BIGGEST
+         *
+         * BUILT-IN OBJECT REFERENCES:
+         * - Byte, Short, Integer
+         * - Long, Float, Double
+         * - Boolean, Character
+         * - String
+         *
+         * WHY DO WE NEED OBJECTS?
+         * - For Integer: to display value in other format
+         *      without storing it in another variable
+         * - Whole concept: for storing and using functions
+         *      of different object types
+         */
+    }
+}
